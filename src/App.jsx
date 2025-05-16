050151a9e06720ffb0dff0b1fd1dbbfc3d06a9ac
Bouchaker ilyes <105610322+ilsbcr@users.noreply.github.com>
2025-05-16 18:17:22 +0100

    create Square component

--- src/App.jsx
+++ src/App.jsx
@@ -32,26 +32,37 @@ export default function App() {
     </>
   )
 }
+
+function Square({ value1 }) {
+
+    const [value, setValue] = useState(null);
+
+    function handleClick() {
+      setValue('X');
+  }
+  return <button className="square" onClick={handleClick}>{value}</button>;
+}
+
 function Board() {
   return (
     <>
-    <div className="board">
-      <div >
-        <button className="square">1</button>
-        <button className="square">2</button>
-        <button className="square">3</button>
+      <div className="board">
+        <div >
+          <Square value={1} />
+          <Square value={2} />
+          <Square value={3} />
+        </div>
+        <div >
+          <Square value={4} />
+          <Square value={5} />
+          <Square value={6} />
+        </div>
+        <div>
+          <Square value={7} />
+          <Square value={8} />
+          <Square value={9} />
+        </div>
       </div>
-      <div >
-        <button className="square">4</button>
-        <button className="square">5</button>
-        <button className="square">6</button>
-      </div>
-      <div>
-        <button className="square">7</button>
-        <button className="square">8</button>
-        <button className="square">9</button>
-      </div>
- </div>
     </>)
 }
 
